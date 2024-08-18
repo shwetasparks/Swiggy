e2d08aacb613d724e1588b823aaf13a2326c81c1
Shweta <shwetakoffficial@gmail.com>
2024-08-18 12:49:20 +0530

    part 6- complete: deals and menu search , starting recommendation  section

--- src/components/RestuarantMenu.jsx
+++ src/components/RestuarantMenu.jsx
@@ -2,37 +2,61 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import greenstar from "../assets/greenstar.png";
+import leftarrow from '../assets/leftarrow.png';
+import rightarrow from '../assets/rightarrow.png';
+import deal from "../assets/deal.png";
+import search from '../assets/search.svg';
 
+function handleNext(){
+   setValue(value + 32);
+}
+function handlePrev(){
+   setValue(value - 32);
+}
+function discount(){
+   return (
+      <div>helo</div>
+   )
+}
 function RestuarantMenu() {
    const { id } = useParams();
+
    // console.log("this is id here",id.split("-").at(-1));
    let mainId = id.split("-").at(-1)
 
    const [menuData, setMenuData] = useState(" ")
    const [resInfo, setResInfo] = useState([])
    const [discountData, setDiscountData] = useState([])
+   const [value,setValue]=useState()
    // console.log(" res info is :-", resInfo);
+// console.log("menu",menuData);
+
+
 
    async function fetchMenu() {
       let data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.73390&lng=76.78890&restaurantId=${mainId}&catalog_qa=undefined&submitAction=ENTER `)
       let res = await data.json();
-
-      console.log(res?.data?.cards[4]?.groupedCard?.groupedCard?.cardGroupedMap?.REGULAR?.cards[1]?.card?.card);
       setResInfo(res?.data?.cards[2]?.card?.card?.info)
-      setMenuData(res?.data?.cards[4]?.groupedCard?.groupedCard?.cardGroupedMap?.REGULAR?.cards[1]?.card?.card?.itemCards)
+
+      let actualMenu=(res?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards).filter((data)=> data?.card?.card?.itemCards)
+      // console.log("actualmenu",actualMenu);
+
+      setMenuData(actualMenu);
       setDiscountData(res?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.offers)
    }
+
    useEffect(() => {
       fetchMenu();
    }, [])
    // console.log(" res info is :-", resInfo);
+  
    return (
-      <div className="w-full  ">
-         <div className="  w-[42%]  mx-auto py-6 px-2">
+      <div className="w-full   ">
+         <div className="  w-[42%]  mx-auto py-6 px-2 border border-black">
             <p className="font-light text-xs text-gray-500 cursor-pointer"><span className="hover:text-black"><Link to={"/"}><span >Home</span></Link></span>  / <Link to={"/"}> <span className="hover:text-black"> {resInfo.city}</span> </Link> /  <Link to={"/"}><span className="hover:text-black">{resInfo.name} </span></Link></p>
             <h1 className="font-bold pt-10 text-2xl">{resInfo?.name}</h1>
 
-            <div className="w-full h-[230px]  mt-4 rounded-3xl bg-gradient-to-t from-gray-300/70 px-4 pb-4  ">
+            <div className="w-full h-[230px]  mt-4 rounded-3xl bg-gradient-to-t from-gray-300/70 px-4 pb-4 my-6  ">
                 <div className="w-full border border-gray-300 rounded-3xl bg-white h-full ">
                   <div className="p-4 w-full ">
                       <div className="flex justify-start gap-3 items-center font-bold text-xl ">
@@ -61,18 +85,85 @@ function RestuarantMenu() {
                   </div>
                   </div>
                 <hr className="" /> 
-                <div className="flex justify-start items-center p-3 ">
+                <div className="flex justify-start items-center p-3 gap-2 ">
                   <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_30,h_30/" + resInfo?.feeDetails?.icon  }/>
                   {
-                      resInfo.length !==0?   <span className="font-semibold text-gray-600">{(resInfo?.expectationNotifiers[0]?.enrichedText)}</span>:""
+                      resInfo.length !==0?   <span className="font-semibold text-gray-600">{(resInfo?.expectationNotifiers[0]?.enrichedText.replace(/<[^>]*>/g,""))}</span>:""
                   }
                 </div>  
                 </div>
             </div>
+
+            {/* Deals for you (on your mind) */}
+            <div className="w-full overflow-hidden">
+            <div className="flex justify-between  gap-3 mx-2 border-border-black my-2 ">
+                <p className="text-2xl font-bold mt-4">Deals for you ?</p>
+                <div className="flex gap-4 m-1 ">
+                    <img
+                        onClick={handlePrev}
+                        className={
+                            ` rounded-full  w-8 p-1  cursor-pointer  ` +
+                            (value <= 0 ? 'text-red-300' : 'text-gray-500')
+                        }
+                        src={leftarrow}
+                    />
+                    <img
+                        onClick={handleNext}
+                        className="bg-gray-200 rounded-full w-8 p-1  cursor-pointer"
+                        src={rightarrow}
+                    />
+                </div>
+            </div>
+
+            {/* discount cards */}
+            <div className="flex  ">
+            {
+               discountData.map((data)=>(
+                  <Discount data={data}/>
+               ))
+            }
+            </div>
+            </div>
+
+            {/* menu: search for dishes */}
+            <h2 className="text-center font-medium text-gray-500 mt-10">M E N U</h2>
+            <div className=" rounded-2xl flex items-center justify-center  h-12 bg-gray-200 mt-4 relative hover:cursor-pointer">
+               <p className="font-bold text-gray-500 text-medium ">Search for dishes</p>
+               <img src={search} className="w-8 h-8 absolute right-2 p-2"/>
+            </div>
+
+            {/* recommended section */}
+
+            <div>
+               {
+                  menuData.map(({card:{card:{itemCards,title}}})=>(
+                     <h1>{title}</h1>
+                  ))
+               }
+            </div>
+
+
+
          </div>
       </div>
    )
 }
+
+
+//deals
+function Discount({data:{info:{header,offerLogo,couponCode}}}){
+   //   console.log(info); 
+      return(
+         <div className="flex items-center border border-gray-300 rounded-2xl m-2 min-w-[328px] h-[76px] px-4 gap-2">
+            <img className="w-12 h-12" src={deal}/>
+            <div>
+               <h2 className="font-extrabold">{header}</h2>
+               <p className="font-bold text-gray-400">{couponCode}</p>
+            </div>
+         </div>
+      )
+   }
+
 export default RestuarantMenu;
 
 
--- src/components/h.js
+++
@@ -1,6 +0,0 @@
-console.log('hello');
-
-function add(){
-   let a=67;
-   let b=67;
-}
